915bf0084c05061a6511dff1e15f33b5531af134
Alexscdt <alex.schmidt59@icloud.com>
2024-11-04 00:55:59 +0100

    feat: add dotenv and request api

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+apiKey: str = os.getenv("API_KEY")
+apiUrl: str = os.getenv("API_URL")
+
 def main():
-    print("Hello, World!")
+    headers: dict = {"x-rapidapi-key": apiKey, "Content-Type": "application/json"}
+    response = requests.get(apiUrl, headers=headers)
+
+    if response.status_code == 200:
+        print('Success')
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
